58e57659169a870e93b29fa901680dfde19d8757
RaiBear08 <61160598+RaiBear08@users.noreply.github.com>
2020-03-11 11:57:09 -0400

    initial commit

---
+++ homeproject2/src/main/java/com/home/formaven/MathOperations.java
@@ -0,0 +1,21 @@
+package com.home.formaven;
+
+public class MathOperations {
+
+	public static int add(int i,int j)
+	{
+		return i+j;
+	}
+	public static int subtract(int i,int j)
+	{
+		return i-j;
+	}
+	public static int multiply(int i,int j)
+	{
+		return i*j;
+	}
+	public static int divide(int i,int j)
+	{
+		return i/j;
+	}
+}
